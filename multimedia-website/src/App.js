11515682f8dcd628352c6a26760b8537a55e0fc4
Faith-yiamat <oloserianyiamat@gmail.com>
2024-07-12 09:37:36 +0300

    added videos

--- multimedia-website/src/App.js
+++ multimedia-website/src/App.js
@@ -4,13 +4,18 @@ import './App.css';
 import Footer from './Footer';
 import LandingPage from './LandingPage';
 import Navbar from './NavBar';
+import Video from './Video';
+import {Route } from 'react-router-dom';
+
 
 function App() {
   return (
     <div className="App">
        <Navbar/>
       <LandingPage/>
+      <Video/>
       <Footer/>
+      {/* <Route path=''/> */}
      
       
  
